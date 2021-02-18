5ae05c09e2247f98245bcbfea54f068c21a024d4
dobosipeter <Lofasz13!>
2021-02-18 18:10:55 +0100

    Query by country code implemented.

--- dataprocessor.py
+++ dataprocessor.py
@@ -16,3 +16,22 @@ def get_total():
     print("The number of confirmed cases: {}.\nThe number of recovered: {}.\nThe number of critical: {}.\nThe mumber "
           "of deaths: {}.\nLast updated: {}.\n".format(data['confirmed'], data['recovered'], data['critical'],
                                                        data['deaths'], data['lastUpdate']))
+
+
+def get_latest_country_data_by_code(countrycode):
+    url = "https://covid-19-data.p.rapidapi.com/country/code"
+
+    querystring = {"code": countrycode}
+
+    headers = {
+        'x-rapidapi-key': "79a3faf042msh5370fe793f1dfabp123310jsnc4b60e1d6d0a",
+        'x-rapidapi-host': "covid-19-data.p.rapidapi.com"
+    }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    data = json.loads(response.text[1:-1])
+    print("The name of the country: {}\nThe number of confirmed cases: {}.\nThe number of recovered: {}.\nThe number "
+          "of critical: {}.\nThe mumber "
+          "of deaths: {}.\nLast updated: {}.\n".format(data['country'], data['confirmed'], data['recovered'], data['critical'],
+                                                       data['deaths'], data['lastUpdate']))
--- menu.py
+++ menu.py
@@ -4,11 +4,16 @@ import dataprocessor
 def main():
     while True:
 
-        choice = input('Choose a menupoint!\nYour choices:\n0. Exit\n1. Get world total\nYour choice: ')
+        choice = input('Choose a menupoint!\nYour choices:\n0. Exit\n1. Get world data\n2. Get data by '
+                       'country code\nYour choice: ') 
         if choice == '0':
             break
         elif choice == '1':
             dataprocessor.get_total()
+        elif choice =='2':
+            dataprocessor.get_latest_country_data_by_code(input("Enter the country code of the country you are "
+                                                                "interested in. Country code is in ISO 3166-1 "
+                                                                "standard.\n"))
         else:
             print('in the menu')
 
